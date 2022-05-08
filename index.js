cc553df26131026ff9817924faa39138d8e712bf
fahad09fahim <anik.fahim07@gmail.com>
2022-05-08 21:15:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhfe1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("db connected");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("running server");
+});
+
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
